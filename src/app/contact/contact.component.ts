714414d535b229e568bcd6e52c8ed539efec2d43
Dmitriy Lukianchuk <dmitriy.lukianchuk@lazada.com>
2017-04-19 00:57:37 +0300

    Generate scheme raw json

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import CustomValidators from '../forms/CustomValidators';
+import schemes from './contact.schemes';
 
 @Component({
   selector: 'app-contact',
@@ -9,6 +10,10 @@ import CustomValidators from '../forms/CustomValidators';
 })
 export class ContactComponent implements OnInit {
   contactForm: FormGroup;
+  scheme: any[];
+  actions: {};
+  positionAbilities: {};
+  positions: {};
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
@@ -17,8 +22,125 @@ export class ContactComponent implements OnInit {
       email: ['', [Validators.required, CustomValidators.validateEmail]],
       content: ['', [Validators.required, Validators.minLength(10)]]
     });
+
+    this.actions = this.getActions();
+    this.positions = schemes.positions;
+    this.positionAbilities = this.getPositionAbilities(schemes.actions);
+
+    this.scheme = this.generateScheme(20);
   }
   submitForm(): void {
     console.log(this.contactForm);
+  }
+  generateScheme(actionNumber) {
+    var usedActions = {};
+
+    var scheme: any[] = [];
+    var schemeActions: any[] = [];
+
+    var currentPosition = schemes.startPosition;
+    scheme.push({
+        position: currentPosition,
+        image_url: ""
+    });
+
+    for(var i=0; i < actionNumber; i++) {
+        var actions = this.recommendActions(currentPosition, usedActions);
+        var action = this.getRandomElem(actions)
+        usedActions[action] = true;
+
+        var actionDesc = this.actions[action];
+        var end = this.getRandomElem(actionDesc.end);
+        schemeActions.push({
+            begin: currentPosition,
+            action: action,
+            end: end
+        });
+        currentPosition = end;
+
+        scheme.push({
+            action: action,
+            video_url: ""
+        });
+        scheme.push({
+            position: end,
+            image_url: ""
+        });
+    }
+
+    return scheme;
+  }
+  getRandomElem(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+  recommendActions(position, usedActions) {
+    var actions = this.positionAbilities[position];
+
+    actions = actions.filter(action => !usedActions[action]);
+
+    if (actions.length <= 0) {
+        actions = this.positionAbilities[position];
+        actions.forEach(action => {delete usedActions[action]});
+    }
+
+    return actions;
+  }
+  getActions() {
+    var actions = schemes.actions;
+    for (var action in actions) {
+        var desc = actions[action];
+
+        if (desc.as !== undefined) {
+            desc = actions[desc.as];
+        }
+
+        if (desc.begin.constructor !== Array) {
+            desc.begin = [desc.begin];
+        }
+        if (desc.end.constructor !== Array) {
+            desc.end = [desc.end];
+        }
+
+        if (desc.begin === undefined) {
+            console.log("Action '"+action
+                +"' doesn't have begin in description '"+desc+"' ");
+            continue;
+        }
+
+        actions[action] = desc;
+    }
+
+    return actions;
+  }
+  getPositionAbilities(actions) {
+    var positionAbilities = {};
+    for (var action in actions) {
+        var desc = actions[action];
+        var begin = desc.begin;
+
+        for (var i in begin) {
+            var position = begin[i];
+
+            if (positionAbilities[position] === undefined) {
+                positionAbilities[position] = [];
+            }
+
+            positionAbilities[position].push(action);
+        }
+    }
+
+    for (var action in actions) {
+        var desc = actions[action];
+        var end = desc.end;
+
+        for (var i in end) {
+            var position = end[i];
+            if (positionAbilities[position] === undefined) {
+                console.error("Position '"+position+"' has no way out");
+            }
+        }
+    }
+
+    return positionAbilities;
   }
 }
---
+++ src/app/contact/contact.schemes.ts
@@ -0,0 +1,108 @@
+export default class schemes {
+    static startPosition = "straight_grip";
+    static actions = {
+        "under_wing": {
+            "begin": "straight_grip",
+            "end": "straight_grip"
+        },
+        "top_change": {
+            "begin": "straight_grip",
+            "end": "straight_grip"
+        },
+        "lower_change": {
+            "begin": "straight_grip",
+            "end": ["straight_grip", "mirrow_grip"]
+        },
+        "rope": {
+            "begin": "straight_grip",
+            "end": "straight_grip"
+        },
+        "spin": {
+            "begin": "mirrow_grip",
+            "end": "straight_grip"
+        },
+        "openning": {
+            "begin": ["straight_grip", "american_grip"],
+            "end": "opened_position"
+        },
+        "support": {
+            "begin": ["anti_american_grip","straight_grip", "opened_position"],
+            "end": "american_grip"
+        },
+        "drop": {
+            "as": "support"
+        },
+        "americana": {
+            "begin": ["straight_grip", "american_grip"],
+            "end": "american_grip"
+        },
+        "americana_out": {
+            "begin": "american_grip",
+            "end": "straight_grip"
+        },
+        "roll_in": {
+            "begin": "opened_position",
+            "end": "rolled_in"
+        },
+        "roll_out": {
+            "begin": "rolled_in",
+            "end": ["opened_position", "side_grip"]
+        },
+        "roll_back": {
+            "begin": "side_grip",
+            "end": "anti_american_grip"
+        },
+        "swift": {
+            "begin": "swift_position",
+            "end": "straight_grip"
+        },
+        "titanic": {
+            "begin": ["same_hand_opened_position", "shadow_position"],
+            "end": "opened_position"
+        },
+        "telespin": {
+            "begin": "lower_american_grip",
+            "end": "opened_position"
+        },
+        "waltz": {
+            "begin": "lower_american_grip",
+            "end": ["american_grip", "straight_grip", "mirrow_grip"]
+        },
+        "rolling_in_front": {
+            "begin": ["rolled_in", "american_grip"],
+            "end": ["anti_american_grip"]
+        },
+        "complex_rope": {
+            "begin": "straight_grip",
+            "end": ["american_grip", "lower_american_grip", "swift_position",
+                "opened_position", "same_hand_opened_position"]
+        },
+        "rotation_under_hand": {
+            "begin": ["american_grip", "lower_american_grip"],
+            "end": "opened_position"
+        },
+        "shadow": {
+            "begin": "mirrow_grip",
+            "end": "shadow_position"
+        },
+        "shadow_out": {
+            "begin": "shadow_position",
+            "end": "mirrow_grip"
+        },
+        "cross": {
+            "begin": "straight_grip",
+            "end": "straight_grip"
+        }
+    };
+    static positions = {
+        "straight_grip": {
+            "img": ""
+        }
+    };
+    static video = {
+        "action": "",
+        "begin": "",
+        "end": "",
+        "video": ""
+    };
+}
