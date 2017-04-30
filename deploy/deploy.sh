2e11c9ff1a5953b87696e71057ff0f8d6318809f
Dmitriy Lukianchuk <dmitriy.lukianchuk@lazada.com>
2017-04-30 18:18:20 +0300

    Deploy concurrently

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,10 +3,10 @@
 SERVER=root@danceranger.com
 DEST=$SERVER:/var/www/danceranger.com
 
-rsync -avz ./src/index.html         $DEST/index.html
-rsync -avz ./dist/main.bundle.js    $DEST/main.bundle.js
+rsync -avz ./src/index.html         $DEST/index.html &
+rsync -avz ./dist/main.bundle.js    $DEST/main.bundle.js &
 rsync -avz ./src/public/*           $DEST/public/ \
-    --exclude-from=./deploy/rsync.exclude --delete
+    --exclude-from=./deploy/rsync.exclude --delete &
 
 rsync -avz ./etc/nginx/vhosts/danceranger.com/* $SERVER:/etc/nginx/vhosts/danceranger.com/ \
-    --exclude-from=./deploy/rsync.exclude --delete
+    --exclude-from=./deploy/rsync.exclude --delete &
